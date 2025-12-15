17c1d0d2679c46ed1be8e54ff91a2ade74e174a1
CyberAutocreator <cryptoautorecovery@gmail.com>
2025-12-15 22:31:11 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    balance: { type: Number, default: 0.00 },
+    // UPDATE THESE DEFAULTS BELOW
+    currency: { type: String, default: 'USD' }, 
+    symbol: { type: String, default: '$' },
+    accountNumber: { type: String },
+    status: { type: String, default: 'active' },
+    createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', userSchema);
